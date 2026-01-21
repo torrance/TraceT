62d1624273df1a2674709e5a14626c838ca9100b
Torrance Hodgson <torrance@pravic.xyz>
2026-01-21 12:27:12 +0800

    Enable user administration by staff members

--- tracet/rules.py
+++ tracet/rules.py
@@ -5,7 +5,13 @@ import rules
 def is_trigger_owner(user, trigger):
     return trigger.user == user
 
+@rules.predicate
+def always(user):
+    print("Hi there!")
+    return False
 
+
+# Trigger permissions
 rules.add_perm(
     "tracet.view_trigger",
     rules.is_group_member("astronomers") | rules.is_group_member("admin"),
@@ -23,4 +29,19 @@ rules.add_perm(
 )
 rules.add_perm(
     "tracet.retrigger_trigger", rules.is_group_member("admin") | is_trigger_owner
+)
+
+# User administration permissions
+rules.add_perm("auth", rules.is_group_member("admin"))
+rules.add_perm(
+    "auth.view_user", rules.is_group_member("admin")
+)
+rules.add_perm(
+    "auth.add_user", rules.is_group_member("admin")
+)
+rules.add_perm(
+    "auth.change_user", rules.is_group_member("admin")
+)
+rules.add_perm(
+    "auth.delete_user", rules.is_group_member("admin")
 )
