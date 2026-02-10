edf5b847c462e7457234f8f59b4114dd8953e656
Torrance Hodgson <torrance@pravic.xyz>
2026-02-10 13:19:58 +0800

    Provide notice disk size and count in admin view

--- tracet/admin.py
+++ tracet/admin.py
@@ -1,5 +1,4 @@
-from nested_admin import NestedTabularInline, NestedModelAdmin, NestedStackedInline
-
+from django.db import connection
 from django.contrib import admin
 
 # Register your models here.
@@ -8,8 +7,21 @@ from . import models
 
 @admin.register(models.GCNStream)
 class GCNStream(admin.ModelAdmin):
-    list_display = ["topic", "type", "status"]
-    readonly_fields = ["status"]
+    list_display = ["topic", "type", "notice_count", "payload_filesize", "status"]
+
+    @admin.display(description="Payload size [MB]")
+    def payload_filesize(self, obj):
+        # Return the cumulative payload filesize of this topic's associated notices. [MB]
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT SUM(OCTET_LENGTH(payload)) FROM tracet_notice WHERE stream_id = %s",
+                [obj.id],
+            )
+            return cursor.fetchone()[0] / 1e6
+
+    @admin.display(description="Notice count")
+    def notice_count(self, obj):
+        return obj.notices.count()
 
     def has_change_permission(self, request, obj=None):
         return False  # Disable editing existing entries
